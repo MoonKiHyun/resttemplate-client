c0ef6fcf082ad0b8197df4db7b888d6f343b04ce
MoonKiHyun <moonkihyun95@gmail.com>
2024-03-05 16:25:54 +0900

    git init

---
+++ src/main/java/com/sparta/springresttemplateclient/SpringresttemplateclientApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springresttemplateclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringresttemplateclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringresttemplateclientApplication.class, args);
+    }
+
+}
